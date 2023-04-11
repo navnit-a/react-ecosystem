af21d6007939115fb0c02c7d507a14221f882dc6
Navnit Anuth <navnit.anuth@outlook.com>
2023-04-11 18:34:35 +0100

    in progress

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import {hot} from "react-hot-loader";
+
+const App = () => {
+	return (
+		<div className="App">
+			<h1>Hello, World Again</h1>
+		</div>
+	);
+};
+
+export default hot(module)(App);
